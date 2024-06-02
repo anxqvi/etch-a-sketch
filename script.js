b7b890de889e0e36cad2f177b9696b0ccec058ac
anxqvi <133358334+anxqvi@users.noreply.github.com>
2024-06-02 15:49:04 +0400

    Added a button to allow the user to change the size of the grid Made the grid proportional to view height and more flexy

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const container = document.querySelector("#container");
-for (let i = 0; i < 4; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < 4; j++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = getRandomColor();
-        })
-        row.appendChild(square);
+const body = document.body;
+let container = document.querySelector("#container");
+createGrid(container,4);
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    let size = prompt("Enter the number of squares per side for the new grid:");
+    if (!(size > 0) && !(size < 101)) {
+        alert("Invalid size! The grid size must be between 1 to 100");
     }
-    container.appendChild(row);
-}
+    container.remove();
+    container = document.createElement("div");
+    container.id = "container";
+    body.appendChild(container);
+    createGrid(container, size);
+});
 
 function getRandomColor() {
     const letters = "0123456789ABCDEF";
@@ -20,4 +22,21 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
+}
+
+function createGrid(container, size) {
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < size; j++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.style.height = `calc(80vh / ${size})`
+            square.addEventListener("mouseover", () => {
+                square.style.backgroundColor = getRandomColor();
+            })
+            row.appendChild(square);
+        }
+        container.appendChild(row);
+    }
 }
